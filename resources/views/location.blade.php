ad6cb5974e8c683fece1a2c613edd99fa2b11eb4
Ilham Nur <romdhoninuril@gmail.com>
2022-07-03 22:44:46 +0700

    location

---
+++ resources/views/location.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.main')
+
+@section('content')
+
+    <!-- ***** Location Start ***** -->
+    <section class="section" id="features">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="section-heading">
+                        <h2>Choose <em>Location</em></h2>
+                        <img src="assets/images/line-dec.png" alt="waves">
+                        <p>Choose a location according to your preferences. All partners at the location are trusted because they have worked with us.</p>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <ul class="features-items">
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/paralayang-hill.png" alt="First One">
+                            </div>
+                            <div class="right-content">
+                                <h4>Watugupit Parangtritis, Yogyakarta</h4>
+                                <p>To go paragliding, you have to prepare a minimum of IDR 300,000 for one flight.</p>
+                                <a href="#" class="text-button">Discover More</a>
+                            </div>
+                        </li>
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/landing-paralayaang.png" alt="Giri Sembung Kulonprogo">
+                            </div>
+                            <div class="right-content">
+                                <h4>Giri Sembung Kulonprogo</h4>
+                                <p>The paragliding spot here is still relatively new because it only opened around the middle of 2021.</p>
+                                <a href="#" class="text-button">Discover More</a>
+                            </div>
+                        </li>
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/embung.png" alt="third gym training">
+                            </div>
+                            <div class="right-content">
+                                <h4>Embung Batara Sriten</h4>
+                                <p>Embung Sriten is located at an altitude of 950 meters above sea level.</p>
+                                <a href="#" class="text-button">Discover More</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-6">
+                    <ul class="features-items">
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/banyak.png" alt="Gunung Banyak, Batu">
+                            </div>
+                            <div class="right-content">
+                                <h4>Mount Banyak, Batu</h4>
+                                <p>You can also try the paragliding experience here with costs starting from IDR 350 thousand - IDR 500 thousand.</p>
+                                <a href="#" class="text-button">Discover More</a>
+                            </div>
+                        </li>
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/Agrowisata_Gunung_Mas.png" alt="Gunung Mas Bogor">
+                            </div>
+                            <div class="right-content">
+                                <h4>Gunung Mas Bogor</h4>
+                                <p>This mountain has many visitors. You can try paragliding with a price range of Rp. 300 thousand - Rp. 500 thousand.</p>
+                                <a href="#" class="text-button">Discover More</a>
+                            </div>
+                        </li>
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/karang bolong.png" alt="Karang Bolong Sukabumi">
+                            </div>
+                            <div class="right-content">
+                                <h4>Karang Bolong Sukabumi</h4>
+                                <p>You will be charged around Rp. 400 thousand - Rp. 600 thousand to try to fly paragliding here.</p>
+                                <a href="#" class="text-button">Discover More</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Location End ***** -->
+
+@endsection    
